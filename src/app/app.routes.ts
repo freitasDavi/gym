61da08ca83fc233086cdbfd57652378c3c0be6cc
Davi <davi.frrs@outlook.com>
2025-09-11 09:58:13 -0300

    new file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { CalisthenicsComponent } from './pages/treino/calisthenics/calisthenics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'calisthenics',
+        component: CalisthenicsComponent,
+      },
+    ],
+  },
+];
---
+++ src/app/pages/treino/calisthenics/calisthenics.component.ts
@@ -0,0 +1,92 @@
+import { Component, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { CardComponent } from '../../../components/card/card.component';
+
+export type Exercicio = {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  numberOfExecutions: number;
+  completed: boolean;
+};
+
+@Component({
+  selector: 'app-calisthenics',
+  imports: [CardComponent, FormsModule],
+  templateUrl: './calisthenics.component.html',
+  styleUrl: './calisthenics.component.css',
+})
+export class CalisthenicsComponent {
+  title = 'gym';
+  tempoDeDescanso = 3;
+  estaDescansando = signal(false);
+
+  exercicios: Exercicio[] = [
+    {
+      id: 1,
+      title: 'Flexão Padrão 5 x 13',
+      description: 'Algo sobre o treino',
+      numberOfExecutions: 5,
+      image:
+        'https://www.hipertrofia.org/blog/wp-content/uploads/2023/11/wide-hand-push-up.gif',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Squat 90º 3 x 3 negativa',
+      description: 'Faz exercicio, segura no fim da execução por 3 segundos.',
+      numberOfExecutions: 3,
+      image:
+        'https://i.pinimg.com/originals/9e/40/fd/9e40fd0f60665b4acb6a53be828909d8.gif',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Squat 90º 3 x 15',
+      description: 'Algo sobre o treino',
+      numberOfExecutions: 3,
+      image:
+        'https://i.pinimg.com/originals/9e/40/fd/9e40fd0f60665b4acb6a53be828909d8.gif',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Elevated Hip Thrust 3 x 15',
+      description: 'Algo sobre o treino',
+      numberOfExecutions: 3,
+      image: 'https://gymvisual.com/img/p/1/0/1/0/6/10106.gif',
+      completed: false,
+    },
+    {
+      id: 5,
+      title: 'Prancha Abdominal 4 x 15',
+      description: 'Algo sobre o treino',
+      numberOfExecutions: 4,
+      image:
+        'https://grandeatleta.com.br/wp-content/uploads/2018/06/Prancha-abdominal-Ponte-ventral.gif',
+      completed: false,
+    },
+  ];
+
+  iniciarDescanso() {
+    let tempo = this.tempoDeDescanso * 60;
+    this.estaDescansando.set(true);
+    const intervalo = setInterval(() => {
+      if (tempo > 0) {
+        tempo--;
+        this.tempoDeDescanso = tempo;
+      } else {
+        clearInterval(intervalo);
+        this.tempoDeDescanso = 3; // Resetar para o valor inicial
+        this.estaDescansando.set(false);
+      }
+    }, 1000);
+  }
+
+  atualizaTempoDeDescanso(valor?: string) {
+    console.log('atualizando ' + valor);
+    if (valor) this.tempoDeDescanso = Number(valor);
+  }
+}
